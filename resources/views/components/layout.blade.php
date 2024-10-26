df02c98e5ca0918e86ac6eda1084003db63ca494
Domenico Pettinato <166862347+Domenico-Pettinato@users.noreply.github.com>
2024-10-26 18:13:50 +0200

    first commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>BariPost</title>
+</head>
+<body>
+    <x-navbar/>
+    <main>
+    <div class="min-vh-100">
+        {{ $slot }}
+        </div>
+    </main>
+    <x-footer/>
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <div class="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 class="text-5xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white md:text-[5rem]">Laravel</h1>
+
+        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+            <h1 class="display-1">Ssc Bari Post</h1>
+        </div>
+    </div>
+</x-layput>
