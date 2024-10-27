f3992f6f5d1e2fd3a871d0c78c229c94465fcd29
Domenico Pettinato <166862347+Domenico-Pettinato@users.noreply.github.com>
2024-10-27 22:11:29 +0100

    laravel fortify completo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,44 @@
 <x-layout>
-    <form method="post" action="{{ route('login')}}">
-        <h1>Login</h1>
-        <input type="text" id="username" placeholder="Username" name="username">
-        <input type="password" id="password" placeholder="Password" name="password">
-        <button type="submit" name="login">Accedi</button>
-    </form>
+    <!-- validazione del login -->
+    @if ($errors->any())
+    <div class=" alert alert-danger">
+        @foreach ($errors->all() as $error)
+        {{ $error }}
+        @endforeach
+    </div>
+    @endif
+
+    <div class="col-12 mt-5 d-flex justify-content-center">
+        <form method="POST" action="{{route('login')}}" class="form">
+            @csrf
+            <div class="container">
+                <h1 class="h3 mb-3 fw-normal text-black">Sign In</h1>
+
+                <div class="form-floating mb-3">
+                    <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Email address</label>
+                </div>
+
+                <div class="form-floating mb-3">
+                    <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Password</label>
+                </div>
+
+                <div class="form-check text-start my-3">
+                    <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+                    <label class="form-check-label text-black" for="flexCheckDefault">Remember me</label>
+                </div>
+                <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+                <p class="mt-3 mb-3 text-black"></p>
+
+            </div>
+
+            <!-- Messaggio di successo -->
+            @if (session('success'))
+            <div class="alert alert-success mt-1" role="alert">
+                {{ session('success') }}
+            </div>
+            @endif
+        </form>
+    </div>
 </x-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,26 +1,48 @@
 <x-layout>
-    <form method="post" action="{{route('register')}}">
-        @csrf
-        <h1>Registrazione</h1>
-        <input type="text" id="username" placeholder="Username" name="username" value="{{ old('username') }}" required>
-        @error('username')
-        <div style="color:red">{{ $message }}</div>
-        @enderror
-
-        <input type="password" id="password" placeholder="Password" name="password" required>
-        @error('password')
-        <div style="color:red">{{ $message }}</div>
-        @enderror
-        <input type="password" id="password_confirmation" placeholder="Conferma Password" name="password_confirmation" required>
-
-        <input type="email" id="email" placeholder="Email" name="email" value="{{ old('email') }}" required>
-        @error('email')
-        <div style="color:red">{{ $message }}</div>
-        @enderror
-
-        <button type="submit" name="register">Registrati</button>
-        @if (session('success'))
-        <div style="color:green">{{ session('success') }}</div>
+<!-- validazione della registrazione -->
+<div class="col-12 mt-5 d-flex justify-content-center ">
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
         @endif
-    </form>
+        <form action="{{route('register')}}" method="POST" class="form">
+            @csrf
+            <div class="container">
+                <h1 class="h3 mb-3 fw-normal text-black"></h1>
+                <p class="mt-2 mb-4 text-black"></p>
+
+                <div class="form-floating mb-3 ">
+                    <input type="text" name="name" class="form-control" id="Name" placeholder="Enter Full Name" required>
+                    <label for="Name">Full Name</label>
+                </div>
+
+                <div class="form-floating mb-3">
+                    <input type="text" placeholder="Enter Email" name="email" class="form-control" id="email" required>
+                   <label for="email">Email</label>
+                </div>
+
+                <div class="form-floating mb-3">
+                    <input type="password" placeholder="Enter Password" name="password" class="form-control" id="password" required>
+                    <label for="password">Password</label>
+                </div>
+
+                <div class="form-floating mb-3">
+                    <input type="password" placeholder="Repeat Password" name="password_confirmation" class="form-control" id="password_confirmation" required>
+                    <label for="password_confirmation">Repeat Password</label>
+                </div>
+
+                <div class="form-floating mb-3">
+                    <p class="mt-3 mb-3 text-white">By creating an account you agree to our <a href="#" class="text-white">Terms & Privacy</a></p>
+                    <button type="submit" class="btn btn-primary w-100 py-2">Register</button>
+                    <p class="mt-3 mb-3 text-black"></p>
+                </div>
+            </div>
+        </form>
+    </div>
 </x-layout>
+
