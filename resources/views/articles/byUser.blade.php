9cb42fee03db1c2506f3969c290a6089a3bcb6cb
Domenico Pettinato <166862347+Domenico-Pettinato@users.noreply.github.com>
2024-11-03 16:48:47 +0100

    fix byuser

--- resources/views/articles/byUser.blade.php
+++ resources/views/articles/byUser.blade.php
@@ -1,25 +1,26 @@
-<x-layout>      
-    <div class="container mt-5 flex flex-col items-center justify-center gap-12 px-4 py-16">                
-        <h1 class="text-5xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white md:text-[5rem]">Laravel</h1>
+<x-layout>
+    <div class="container mt-5 text-center">
 
         <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <h1 class="display-1">Ssc Bari Post</h1>
             <h2 class="display-2">Articoli dell utente "{{$user->name}}"</h2>
         </div>
         <!-- card -->
-        @foreach ($articles as $article)
-        <div class="container mt-5 ">
-            <div class="card" style="width: 18rem; border: 1px solid #ccc">
-                <img src="https://picsum.photos/{{ 300 + $article->id }}" class="card-img-top" alt="Immagine di esempio">
-                <div class="card-body">
-                    <h5 class="card-title">{{$article->title}}</h5>
-                    <p class="card-text">Categoria: {{$article->category->name}}</p> 
-                    <p class="card-text">Autore: {{$article->user->name ?? 'Autore non disponibile'}}</p>
-                    <p class="card-text">{{$article->body}}.</p>
-                    <a href="{{ route('articles.show', ['article' => $article->id]) }}" class="btn btn-primary">View Details</a>
+        <div class="container mt-5">
+        <div class="row">
+            @foreach ($articles as $article)
+            <div class="col-md-6 col-12 mb-4 d-flex justify-content-center">
+                <div class="card" style="width: 18rem; border: 1px solid #ccc">
+                    <img src="https://picsum.photos/{{ 300 + $article->id }}" class="card-img-top" alt="Immagine di esempio">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $article->title }}</h5>
+                        <p class="card-text">Categoria: {{ $article->category->name }}</p>
+                        <p class="card-text">Autore: {{ $article->user->name }}</p>
+                        <p class="card-text">{{ $article->body }}.</p>
+                        <a href="{{ route('articles.show', ['article' => $article->id]) }}" class="btn btn-primary">Dettaglio Articolo</a>
+                    </div>
                 </div>
             </div>
+            @endforeach
         </div>
-        @endforeach
-    </div>  
+    </div>
 </x-layout>
